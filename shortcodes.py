6f92d9c7c7b5dca8c55c5bcea5331c6d355b84fb
Darren Mulholland <dmulholland@outlook.ie>
2016-01-06 23:01:57 +0000

    Allow registering handlers on parser instances

--- shortcodes.py
+++ shortcodes.py
@@ -11,13 +11,14 @@ Innermost shortcodes are processed first.
 String escapes inside quoted arguments are decoded; unquoted arguments
 are preserved in their raw state.
 
-Register handler functions using the @register decorator:
+Handler functions can be registered globally using the @register decorator
+or locally on an individual parser instance using its register() method:
 
     @shortcodes.register('tag')
     def handler(context=None, content=None, pargs=[], kwargs={}):
         ...
 
-Specify an end-tag to create a shortcode with block scope:
+Specifying an end-tag creates a shortcode with block scope:
 
     @shortcodes.register('tag', 'endtag')
     def handler(context=None, content=None, pargs=[], kwargs={}):
@@ -44,7 +45,7 @@ A single Parser() object can process multiple input strings. The optional
 `context` argument accepts an arbitrary object to pass on to the registered
 handler functions.
 
-Author: Darren Mulholland <dmulholland@outlook.ie>
+Author: Darren Mulholland <darren@mulholland.xyz>
 License: Public Domain
 
 """
@@ -54,27 +55,31 @@ import sys
 
 
 # Library version number.
-__version__ = "2.0.2"
+__version__ = "2.1.0.dev"
 
 
-# Stores registered shortcode functions indexed by tag.
-tagmap = { 'endtags': [] }
+# Globally registered shortcode functions indexed by tag.
+globaltags = {}
+
+
+# Globally registered end-tags for block-scoped shortcodes.
+globalends = []
 
 
 def register(tag, end_tag=None):
-    """ Decorator for registering shortcode functions. """
+    """ Decorator for globally registering shortcode functions. """
 
     def register_function(function):
-        tagmap[tag] = {'func': function, 'endtag': end_tag}
+        globaltags[tag] = {'func': function, 'endtag': end_tag}
         if end_tag:
-            tagmap['endtags'].append(end_tag)
+            globalends.append(end_tag)
         return function
 
     return register_function
 
 
 def decode(s):
-    """ Decodes string escape sequences. """
+    """ Decode string escape sequences. """
     return bytes(s, 'utf-8').decode('unicode_escape')
 
 
@@ -89,7 +94,7 @@ class NestingError(ShortcodeError):
 
 
 class InvalidTagError(ShortcodeError):
-    """ Raised if the parser encounters an unknown tag. """
+    """ Raised if the parser encounters an unrecognised tag. """
     pass
 
 
@@ -111,7 +116,7 @@ class Node:
 
 class Text(Node):
 
-    """ Text content outside and between shortcode tags. """
+    """ Plain text. """
 
     def __init__(self, text):
         self.text = text
@@ -137,20 +142,25 @@ class Shortcode(Node):
         (\S+)
     """, re.VERBOSE)
 
-    def __init__(self, tag, argstring):
+    def __init__(self, tag, args, func):
         self.tag = tag
-        self.func = tagmap[tag]['func']
-        self.pargs, self.kwargs = self.parse_args(argstring)
+        self.func = func
+        self.pargs, self.kwargs = self.parse_args(args)
 
     def render(self, context):
         try:
-            return str(self.func(context, None, self.pargs, self.kwargs))
-        except Exception as e:
-            raise RenderingError('error rendering [%s] tag' % self.tag)
+            return str(self.func(
+                context,
+                None,
+                self.pargs,
+                self.kwargs
+            ))
+        except:
+            raise RenderingError('error rendering [%s] shortcode' % self.tag)
 
-    def parse_args(self, argstring):
+    def parse_args(self, args):
         pargs, kwargs = [], {}
-        for match in self.argregex.finditer(argstring):
+        for match in self.argregex.finditer(args):
             if match.group(2) or match.group(5):
                 key = match.group(1) or match.group(5)
                 value = match.group(3) or match.group(4) or match.group(6)
@@ -169,16 +179,22 @@ class ScopedShortcode(Shortcode):
 
     """ Block-scoped shortcode (a shortcode with opening and closing tags). """
 
-    def __init__(self, tag, argstring):
-        Shortcode.__init__(self, tag, argstring)
+    def __init__(self, tag, args, func):
+        self.tag = tag
+        self.func = func
+        self.pargs, self.kwargs = self.parse_args(args)
         self.children = []
 
     def render(self, context):
-        content = ''.join(child.render(context) for child in self.children)
         try:
-            return str(self.func(context, content, self.pargs, self.kwargs))
+            return str(self.func(
+                context,
+                ''.join(child.render(context) for child in self.children),
+                self.pargs,
+                self.kwargs
+            ))
         except:
-            raise RenderingError('error rendering [%s] tag' % self.tag)
+            raise RenderingError('error rendering [%s] shortcode' % self.tag)
 
 
 class Parser:
@@ -197,7 +213,7 @@ class Parser:
 
     def __init__(self, start='[%', end='%]', esc='\\'):
         self.start = start
-        self.estart = esc + start
+        self.esc_start = esc + start
         self.len_start = len(start)
         self.len_end = len(end)
         self.len_esc = len(esc)
@@ -206,54 +222,85 @@ class Parser:
             re.escape(start),
             re.escape(end),
         ))
+        self.tags = {}
+        self.ends = []
 
-    def parse(self, text, context=None):
-        stack, expecting = [Node()], []
+    def register(self, func, tag, end_tag=None):
+        self.tags[tag] = {'func': func, 'endtag': end_tag}
+        if end_tag:
+            self.ends.append(end_tag)
 
-        for token in self._tokenize(text):
-            if token.startswith(self.start):
-                content = token[self.len_start:-self.len_end].strip()
-                if content:
-                    self._parse_token_content(stack, expecting, content)
-            elif token.startswith(self.estart):
-                stack[-1].children.append(Text(token[self.len_esc:]))
-            else:
-                stack[-1].children.append(Text(token))
-
-        if expecting:
-            raise NestingError('expecting [%s]' % expecting[-1])
-
-        return stack.pop().render(context)
-
-    def _tokenize(self, text):
+    def tokenize(self, text):
         for token in self.regex.split(text):
             if token:
                 yield token
 
-    def _parse_token_content(self, stack, expecting, content):
-        tagstr = content.split(None, 1)[0]
-        argstr = content[len(tagstr):]
+    def parse(self, text, context=None):
 
-        if tagstr in tagmap['endtags']:
+        # Local, merged copies of the global and parser tag registries.
+        tags = globaltags.copy()
+        tags.update(self.tags)
+        ends = globalends[:] + self.ends
+
+        # Stack of in-scope nodes and their expected end-tags.
+        stack, expecting = [Node()], []
+
+        # Process the input stream of tokens.
+        for token in self.tokenize(text):
+            self.parse_token(token, stack, expecting, tags, ends)
+
+        # The stack of expected end-tags should finish empty.
+        if expecting:
+            raise NestingError('expecting [%s]' % expecting[-1])
+
+        # Pop the root node and render it as a string.
+        return stack.pop().render(context)
+
+    def parse_token(self, token, stack, expecting, tags, ends):
+
+        # Do we have a shortcode token?
+        if token.startswith(self.start):
+            content = token[self.len_start:-self.len_end].strip()
+            if content:
+                self.parse_shortcode(content, stack, expecting, tags, ends)
+
+        # Do we have an escaped shortcode token?
+        elif token.startswith(self.esc_start):
+            stack[-1].children.append(Text(token[self.len_esc:]))
+
+        # We must have a text token.
+        else:
+            stack[-1].children.append(Text(token))
+
+    def parse_shortcode(self, content, stack, expecting, tags, ends):
+
+        # Split the content into the tag and argument string.
+        tag = content.split(None, 1)[0]
+        args = content[len(tag):]
+
+        # Do we have a registered end-tag?
+        if tag in ends:
             if not expecting:
-                raise NestingError('not expecting [%s]' % tagstr)
-            elif tagstr == expecting[-1]:
+                raise NestingError('not expecting [%s]' % tag)
+            elif tag == expecting[-1]:
                 stack.pop()
                 expecting.pop()
             else:
                 msg = 'expecting [%s], found [%s]'
-                raise NestingError(msg % (expecting[-1], tagstr))
+                raise NestingError(msg % (expecting[-1], tag))
 
-        elif tagstr in tagmap:
-            if tagmap[tagstr]['endtag']:
-                node = ScopedShortcode(tagstr, argstr)
+        # Do we have a registered tag?
+        elif tag in tags:
+            if tags[tag]['endtag']:
+                node = ScopedShortcode(tag, args, tags[tag]['func'])
                 stack[-1].children.append(node)
                 stack.append(node)
-                expecting.append(tagmap[tagstr]['endtag'])
+                expecting.append(tags[tag]['endtag'])
             else:
-                node = Shortcode(tagstr, argstr)
+                node = Shortcode(tag, args, tags[tag]['func'])
                 stack[-1].children.append(node)
 
+        # We have an unrecognised tag.
         else:
             msg = '[%s] is not a recognised shortcode tag'
-            raise InvalidTagError(msg % tagstr)
+            raise InvalidTagError(msg % tag)
