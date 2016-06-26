26e26db6b9c424c0e6786b431a149ddbf504308e
Darren Mulholland <darren@mulholland.xyz>
2016-06-26 20:09:02 +0100

    Refactor code for clarity and style

--- shortcodes.py
+++ shortcodes.py
@@ -1,64 +1,19 @@
-"""
-A generic, customizable shortcode parser.
-
-Parses shortcodes of the form:
-
-    [% tag arg1 'arg 2' key1=arg3 key2='arg 4' %] ... [% endtag %]
-
-Shortcodes can be atomic or block-scoped and can be nested to any depth.
-Innermost shortcodes are processed first.
-
-String escapes inside quoted arguments are decoded; unquoted arguments
-are preserved in their raw state.
-
-Handler functions can be registered globally using the @register decorator
-or locally on an individual parser instance using its register() method:
-
-    @shortcodes.register('tag')
-    def handler(context=None, content=None, pargs=[], kwargs={}):
-        ...
-
-Specifying an end-tag creates a shortcode with block scope:
-
-    @shortcodes.register('tag', 'endtag')
-    def handler(context=None, content=None, pargs=[], kwargs={}):
-        ...
-
-Handler functions should accept four arguments:
-
-    `context`: an arbitrary context object
-    `content`: the shortcode's content as a string in the case of block-scoped
-               shortcodes, or None in the case of atomic shortcodes
-    `pargs`:   a list of the shortcode's positional arguments
-    `kwargs`:  a dictionary of the shortcode's keyword arguments
-
-Positional and keyword arguments are passed as strings. The handler function
-itself should return a string.
-
-To parse an input string containing shortcodes, create a Parser() object and
-call its parse() method:
-
-    parser = shortcodes.Parser()
-    output = parser.parse(text, context=None)
-
-A single Parser() object can process multiple input strings. The optional
-`context` argument accepts an arbitrary object to pass on to the registered
-handler functions.
-
-Author: Darren Mulholland <darren@mulholland.xyz>
-License: Public Domain
-
-"""
+# --------------------------------------------------------------------------
+# A library for parsing customizable Wordpress-style shortcodes.
+#
+# Author: Darren Mulholland
+# License: Public Domain
+# --------------------------------------------------------------------------
 
 import re
 import sys
 
 
 # Library version number.
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 
 
-# Globally registered shortcode functions indexed by tag.
+# Globally registered shortcode handlers indexed by tag.
 globaltags = {}
 
 
@@ -66,8 +21,8 @@ globaltags = {}
 globalends = []
 
 
+# Decorator function for globally registering shortcode handlers.
 def register(tag, end_tag=None):
-    """ Decorator for globally registering shortcode functions. """
 
     def register_function(function):
         globaltags[tag] = {'func': function, 'endtag': end_tag}
@@ -78,34 +33,43 @@ def register(tag, end_tag=None):
     return register_function
 
 
-def decode(s):
-    """ Decode string escape sequences. """
+# Decode unicode escape sequences in a string.
+def decode_escapes(s):
     return bytes(s, 'utf-8').decode('unicode_escape')
 
 
+# --------------------------------------------------------------------------
+# Exception classes.
+# --------------------------------------------------------------------------
+
+
+# Base class for all exceptions raised by the library.
 class ShortcodeError(Exception):
-    """ Base class for all exceptions raised by the library. """
     pass
 
 
+# Exception raised if the parser detects unbalanced tags.
 class NestingError(ShortcodeError):
-    """ Raised if the parser detects unbalanced tags. """
     pass
 
 
+# Exception raised if the parser encounters an unrecognised tag.
 class InvalidTagError(ShortcodeError):
-    """ Raised if the parser encounters an unrecognised tag. """
     pass
 
 
+# Exception raised if an attempt to call a shortcode function fails.
 class RenderingError(ShortcodeError):
-    """ Raised if an attempt to call a shortcode function fails. """
     pass
 
 
-class Node:
+# --------------------------------------------------------------------------
+# AST Nodes.
+# --------------------------------------------------------------------------
 
-    """ Input text is parsed into a tree of Node objects. """
+
+# Input text is parsed into a tree of Node instances.
+class Node:
 
     def __init__(self):
         self.children = []
@@ -114,9 +78,8 @@ class Node:
         return ''.join(child.render(context) for child in self.children)
 
 
+# A Text node represents plain text located between shortcode tokens.
 class Text(Node):
-
-    """ Plain text. """
 
     def __init__(self, text):
         self.text = text
@@ -125,11 +88,13 @@ class Text(Node):
         return self.text
 
 
+# Base class for atomic and block-scoped shortcodes. Note that string escapes
+# inside quoted arguments are decoded; unquoted arguments are preserved in
+# their raw state.
 class Shortcode(Node):
 
-    """ Atomic shortcode (a shortcode with no closing tag). """
-
-    argregex = re.compile(r"""
+    # Regex for parsing the shortcode's arguments.
+    re_args = re.compile(r"""
         (?:([^\s'"=]+)=)?
         (
             "((?:[^\\"]|\\.)*)"
@@ -142,30 +107,20 @@ class Shortcode(Node):
         (\S+)
     """, re.VERBOSE)
 
-    def __init__(self, tag, args, func):
+    def __init__(self, tag, argstring, func):
         self.tag = tag
         self.func = func
-        self.pargs, self.kwargs = self.parse_args(args)
+        self.pargs, self.kwargs = self.parse_args(argstring)
+        self.children = []
 
-    def render(self, context):
-        try:
-            return str(self.func(
-                context,
-                None,
-                self.pargs,
-                self.kwargs
-            ))
-        except:
-            raise RenderingError('error rendering [%s] shortcode' % self.tag)
-
-    def parse_args(self, args):
+    def parse_args(self, argstring):
         pargs, kwargs = [], {}
-        for match in self.argregex.finditer(args):
+        for match in self.re_args.finditer(argstring):
             if match.group(2) or match.group(5):
                 key = match.group(1) or match.group(5)
                 value = match.group(3) or match.group(4) or match.group(6)
                 if match.group(3) or match.group(4):
-                    value = decode(value)
+                    value = decode_escapes(value)
                 if key:
                     kwargs[key] = value
                 else:
@@ -175,41 +130,37 @@ class Shortcode(Node):
         return pargs, kwargs
 
 
-class ScopedShortcode(Shortcode):
-
-    """ Block-scoped shortcode (a shortcode with opening and closing tags). """
-
-    def __init__(self, tag, args, func):
-        self.tag = tag
-        self.func = func
-        self.pargs, self.kwargs = self.parse_args(args)
-        self.children = []
+# An atomic shortcode is a shortcode with no closing tag.
+class AtomicShortcode(Shortcode):
 
     def render(self, context):
         try:
-            return str(self.func(
-                context,
-                ''.join(child.render(context) for child in self.children),
-                self.pargs,
-                self.kwargs
-            ))
+            return str(self.func(context, None, self.pargs, self.kwargs))
         except:
             raise RenderingError('error rendering [%s] shortcode' % self.tag)
 
 
+# A block-scoped shortcode is a shortcode with a closing tag.
+class BlockShortcode(Shortcode):
+
+    def render(self, context):
+        try:
+            content = ''.join(child.render(context) for child in self.children)
+            return str(self.func(context, content, self.pargs, self.kwargs))
+        except:
+            raise RenderingError('error rendering [%s] shortcode' % self.tag)
+
+
+# --------------------------------------------------------------------------
+# Parser.
+# --------------------------------------------------------------------------
+
+
+# A Parser instance parses input text and renders shortcodes. A single Parser
+# instance can parse an unlimited number of input strings. Note that the
+# parse() method accepts an arbitrary context object which it passes on to
+# each shortcode's handler function.
 class Parser:
-
-    """ Parses text and renders shortcodes.
-
-    A single Parser instance can parse multiple input strings.
-
-        parser = Parser()
-        output = parser.parse(text, context)
-
-    The parse() method accepts an arbitrary context object which it
-    passes on to the shortcode handler functions.
-
-    """
 
     def __init__(self, start='[%', end='%]', esc='\\'):
         self.start = start
@@ -230,11 +181,6 @@ class Parser:
         if end_tag:
             self.ends.append(end_tag)
 
-    def tokenize(self, text):
-        for token in self.regex.split(text):
-            if token:
-                yield token
-
     def parse(self, text, context=None):
 
         # Local, merged copies of the global and parser tag registries.
@@ -246,8 +192,8 @@ class Parser:
         stack, expecting = [Node()], []
 
         # Process the input stream of tokens.
-        for token in self.tokenize(text):
-            self.parse_token(token, stack, expecting, tags, ends)
+        for token in self._tokenize(text):
+            self._parse_token(token, stack, expecting, tags, ends)
 
         # The stack of expected end-tags should finish empty.
         if expecting:
@@ -256,13 +202,18 @@ class Parser:
         # Pop the root node and render it as a string.
         return stack.pop().render(context)
 
-    def parse_token(self, token, stack, expecting, tags, ends):
+    def _tokenize(self, text):
+        for token in self.regex.split(text):
+            if token:
+                yield token
+
+    def _parse_token(self, token, stack, expecting, tags, ends):
 
         # Do we have a shortcode token?
         if token.startswith(self.start):
             content = token[self.len_start:-self.len_end].strip()
             if content:
-                self.parse_shortcode(content, stack, expecting, tags, ends)
+                self._parse_sc_token(content, stack, expecting, tags, ends)
 
         # Do we have an escaped shortcode token?
         elif token.startswith(self.esc_start):
@@ -272,11 +223,11 @@ class Parser:
         else:
             stack[-1].children.append(Text(token))
 
-    def parse_shortcode(self, content, stack, expecting, tags, ends):
+    def _parse_sc_token(self, content, stack, expecting, tags, ends):
 
-        # Split the content into the tag and argument string.
+        # Split the token's content into the tag and argument string.
         tag = content.split(None, 1)[0]
-        args = content[len(tag):]
+        argstring = content[len(tag):]
 
         # Do we have a registered end-tag?
         if tag in ends:
@@ -292,12 +243,12 @@ class Parser:
         # Do we have a registered tag?
         elif tag in tags:
             if tags[tag]['endtag']:
-                node = ScopedShortcode(tag, args, tags[tag]['func'])
+                node = BlockShortcode(tag, argstring, tags[tag]['func'])
                 stack[-1].children.append(node)
                 stack.append(node)
                 expecting.append(tags[tag]['endtag'])
             else:
-                node = Shortcode(tag, args, tags[tag]['func'])
+                node = AtomicShortcode(tag, argstring, tags[tag]['func'])
                 stack[-1].children.append(node)
 
         # We have an unrecognised tag.
