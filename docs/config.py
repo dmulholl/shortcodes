7a3ac94fa42594e45508673727083a2c7510d30f
Darren Mulholland <darren@mulholland.xyz>
2018-02-20 00:21:49 +0000

    Add documentation to the repository

---
+++ docs/config.py
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------------------------
+# Site Configuration File
+# ------------------------------------------------------------------------------
+
+title = "Shortcodes"
+
+tagline = "A Python library for parsing customizable WordPress-style shortcodes."
---
+++ docs/inc/menu.md
@@ -0,0 +1,7 @@
+---
+# This is a sample menu file.
+---
+
+* [Home](@root/)
+* [Guide](@root/guide//)
+* [Source](https://github.com/dmulholland/shortcodes)
